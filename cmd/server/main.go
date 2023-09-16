8e3fa2e1148de70e665e5792eb1f3c2c01bd9599
shadow3x3x3 <shadow3x3x3@gmail.com>
2023-09-16 13:35:33 +0800

    feat: add config reader

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"orange-backstage-api/app"
 	"orange-backstage-api/infra/config"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/spf13/viper"
 )
 
 // Constants used for injecting by go build -ldflags
@@ -17,14 +21,47 @@ var (
 	AppBuild   = "unknown_build"
 )
 
+var defaultCfg = config.App{
+	Server: config.Server{
+		RunMode:      "release",
+		Port:         "8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	},
+
+	Log: config.Log{
+		Level:        "info",
+		FileName:     "server.log",
+		MaxSize:      100,
+		MaxBackups:   10,
+		MaxAge:       30,
+		Compress:     true,
+		ConsoleDebug: false,
+	},
+}
+
 func main() {
+	viper.AddConfigPath("./")
+	viper.SetConfigName("server_config")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		var notfoundErr viper.ConfigFileNotFoundError
+		if errors.As(err, &notfoundErr) {
+			log.Println("Config file not found, use default config")
+		} else {
+			log.Fatalf("Error reading config file: %v", err)
+		}
+	}
+
+	appCfg := defaultCfg
+	if err := viper.Unmarshal(&appCfg); err != nil {
+		log.Fatalf("Unable to decode into struct： %v", err)
+	}
+
 	rootCtx, cancel := context.WithCancel(context.Background())
-	app, err := app.New(rootCtx, AppName, config.App{
-		Log: config.Log{
-			Level:        "debug",
-			ConsoleDebug: true,
-		},
-	})
+	app, err := app.New(rootCtx, AppName, appCfg)
 	if err != nil {
 		log.Fatalf("failed to create app: %v", err)
 	}
